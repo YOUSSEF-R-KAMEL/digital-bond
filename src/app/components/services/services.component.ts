58f4f1bd9cbfec2821dc749ca9762c66d8ff7b6a
‪Youssef Raafat‬‏ <126985282+YOUSSEF-R-KAMEL@users.noreply.github.com>
2025-05-07 09:34:33 +0300

    [feat]: complete

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,22 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
 
   servicesList = [
-    { img: 'se_01.png', text: 'BRANDING' },
-    { img: 'se_02.png', text: 'SOCIAL MEDIA' },
-    { img: 'se_03.png', text: 'INFLUENCERS MARKETING & Digital PR' },
-    { img: 'se_04.png', text: 'VIDEO PRODUCTION' },
-    { img: 'se_05.png', text: 'WEB DEVELOPMENT' },
-    { img: 'se_06.png', text: 'SMS CAMPAIGNS' },
-    { img: 'se_07.png', text: 'FOOD STYLING PHOTOGRAPHY' },
-    { img: 'se_08.png', text: 'SEO OPTIMIZATION' },
-    { img: 'se_09.png', text: 'PERFORMANCE MARKETING' }
+    { img: 'se_01.png', text: 'BRANDING', amimatioType: 'fade-down-right' },
+    { img: 'se_02.png', text: 'SOCIAL MEDIA', amimatioType: 'fade-down' },
+    { img: 'se_03.png', text: 'INFLUENCERS MARKETING & Digital PR', amimatioType: 'fade-down-left' },
+    { img: 'se_04.png', text: 'VIDEO PRODUCTION', amimatioType: 'fade-right' },
+    { img: 'se_05.png', text: 'WEB DEVELOPMENT', amimatioType: 'fade-up' },
+    { img: 'se_06.png', text: 'SMS CAMPAIGNS', amimatioType: 'fade-left' },
+    { img: 'se_07.png', text: 'FOOD STYLING PHOTOGRAPHY', amimatioType: 'fade-up-right' },
+    { img: 'se_08.png', text: 'SEO OPTIMIZATION', amimatioType: 'fade-up' },
+    { img: 'se_09.png', text: 'PERFORMANCE MARKETING', amimatioType: 'fade-up-left' }
   ];
 }
---
+++ src/vanta.d.ts
@@ -0,0 +1,5 @@
+// src/types/vanta.d.ts
+declare module 'vanta/dist/vanta.birds.min.js' {
+  const Vanta: any;
+  export default Vanta;
+}
